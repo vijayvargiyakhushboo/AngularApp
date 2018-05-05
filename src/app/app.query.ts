283dbb2018fd49bb166d4a28acc27c2e4df397c9
khushboo <khushboovjvrg018@gmail.com>
2018-05-06 03:00:25 +0530

    import ts into component.ts

--- src/app/app.query.ts
+++ src/app/app.query.ts
@@ -1,12 +1,13 @@
  class Query {
-   constructor(firstName,lastName) {
+   constructor(public firstName: String, public lastName: String) {
    	this.firstName = firstName;
    	this.lastName = lastName;
+   }
         displayName(){
 
        console.log(this.firstName+ " " +this.lastName);
   }
      
-   }
+   
 }
 export const Server = Query;
